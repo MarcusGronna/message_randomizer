08eeb84cc1ca170b8b43ace375c4e12fc696e361
Marcus <mgronna@gmail.com>
2024-08-18 19:36:43 +0200

    Add arrays and randomizer()

---
+++ script.js
@@ -0,0 +1,35 @@
+// Message generator
+const firstPart = ["Whenever you",
+    "If you",
+    "When life",
+    "Every time you",
+    "When you find yourself",
+    "In moments of doubt",
+    "Whenever you feel",
+    "When everything seems",
+    "In times of uncertainty",
+    "When you need"]
+const secondPart = ["feel overwhelmed",
+    "need a boost",
+    "are facing challenges",
+    "feel down",
+    "need some encouragement",
+    "are struggling",
+    "feel lost",
+    "need a reminder",
+    "feel unappreciated",
+    "are seeking strength"]
+const thirdPart = ["remember your strength.",
+  "know that you are capable.",
+  "take a deep breath and carry on.",
+  "you are not alone.",
+  "believe in yourself.",
+  "reach out for support.",
+  "find the light within you.",
+  "you are stronger than you think.",
+  "embrace your inner resilience.",
+  "you have the power to overcome."]
+
+  const randomizer = arr => Math.floor(Math.random() * arr.length)
+
+
